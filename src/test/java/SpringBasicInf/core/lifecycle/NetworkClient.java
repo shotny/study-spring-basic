411de253ddd09f62e6c2e76c08667936a2e21957
shotny <yeahh2483@gmail.com>
2022-12-27 11:40:39 +0900

    section8 빈 생명주기 콜백

---
+++ src/test/java/SpringBasicInf/core/lifecycle/NetworkClient.java
@@ -0,0 +1,61 @@
+package SpringBasicInf.core.lifecycle;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+//public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+//        connect();
+//        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: "+url+" message: "+message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: "+ url);
+    }
+
+    // afterPropertiesSet -> 의존관계 주입이 끝나면
+    /* 인터페이스 이용
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }*/
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
